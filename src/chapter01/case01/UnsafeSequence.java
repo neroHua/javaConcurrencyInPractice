1d77359cc05c77848fc7cc38dae426e1ff15b4ec
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-09 15:55:42 +0800

    Update UnsafeSequence.java

--- src/chapter01/case01/UnsafeSequence.java
+++ src/chapter01/case01/UnsafeSequence.java
@@ -3,7 +3,6 @@ package chapter01.case01;
 import common.NotThreadSafe;
 import common.ThreadUnsafeException;
 
-@NotThreadSafe
 public class UnsafeSequence {
     private int value;
 
@@ -11,6 +10,39 @@ public class UnsafeSequence {
         return value;
     }
 
+    /**
+     *   public int getAndSetNextValue();
+     *     Code:
+     *        0: aload_0
+     *        1: dup
+     *        2: getfield      #2                  // Field value:I
+     *        5: dup_x1
+     *        6: iconst_1
+     *        7: iadd
+     *        8: putfield      #2                  // Field value:I
+     *       11: ireturn
+     *
+     *      description:
+     *         0: // Push local variable 0 (this)
+     *         1: // Duplicate the top value on the operand stack and push the duplicated value onto the operand stack.
+     *         2: // Fetch field from object
+     *         6: // Push the int constant <i> (-1, 0, 1, 2, 3, 4 or 5) onto the operand stack.
+     *         7: // Add int
+     *         8: // Set field in object
+     *        11: // Return int from method
+     *
+     *      Operand stack:
+     *          0: ....., this
+     *          1: ....., this, this
+     *          2: ....., this, this.value
+     *          5: ....., this.value, this, this.value
+     *          6: ....., this.value, this, this.value, 1
+     *          7: ....., this.value, this, this.value + 1
+     *          8: ....., this.value
+     *         11: .....,
+     *
+     */
+    @NotThreadSafe
     public int getAndSetNextValue() {
         return value++;
     }
