4d4e32e13d9f8e11395b772637d770c181600226
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-09 13:53:54 +0800

    Create GuardedBy.java

---
+++ src/common/GuardedBy.java
@@ -0,0 +1,15 @@
+package common;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+@Documented
+@Retention(SOURCE)
+@Target({ TYPE, FIELD, METHOD, CONSTRUCTOR, LOCAL_VARIABLE })
+public @interface GuardedBy {
+
+}
