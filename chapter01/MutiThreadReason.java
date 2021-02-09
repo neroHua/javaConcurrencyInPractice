284d1512272358d4dc972ea402c35279c148ba94
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-09 11:12:11 +0800

    Update MutiThreadReason.java

--- chapter01/MutiThreadReason.java
+++ chapter01/MutiThreadReason.java
@@ -1,7 +1,15 @@
 package chapter01;
 
 /**
- * need to find on cpu history
+ * 0. reason:
+ * 0. 原因:
+ * 
+ * 1.it's hard to scale up clock rates in manufacturers
+ * 1. 在加工制造cpu时，很难提升时钟频率。
+ * 
+ * 2.sequential programming can't make full use cpu: shus as waitting for I/O finish
+ * 2. 串行程序不能充分利用cpu资源。
+ * 
  *
  */
 public class MutiThreadReason {
