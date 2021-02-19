91551e24c3a809b7bc0542e0e7a57f6ad8dbbbce
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-19 21:12:26 +0800

    Create ListHelper.java

---
+++ src/chapter04/case11/ListHelper.java
@@ -0,0 +1,25 @@
+package chapter04.case11;
+
+import common.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@ThreadSafe
+public class ListHelper<E> {
+
+    public List<E> list = Collections.synchronizedList(new ArrayList<E>());
+
+    public boolean putIfAbsent(E x) {
+        synchronized (list) {
+            boolean absent = !list.contains(x);
+            if (absent) {
+                list.add(x);
+            }
+
+            return absent;
+        }
+    }
+
+}
