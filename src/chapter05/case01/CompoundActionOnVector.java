e5e1ce0a5924d3551258b53def40cfbeb5e72deb
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-19 21:14:08 +0800

    Create CompoundActionOnVector.java

---
+++ src/chapter05/case01/CompoundActionOnVector.java
@@ -0,0 +1,17 @@
+package chapter05.case01;
+
+import java.util.Vector;
+
+public class CompoundActionOnVector {
+
+    public static Object getLast(Vector list) {
+        int lastIndex = list.size() - 1;
+        return list.get(lastIndex);
+    }
+
+    public static void deleteLast(Vector list) {
+        int lastIndex = list.size() - 1;
+        list.remove(lastIndex);
+    }
+
+}
