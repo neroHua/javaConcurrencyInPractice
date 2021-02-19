e66f03e8c394fbbec200fbd654efe0e4c53ba060
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-19 11:33:35 +0800

    Create Holder.java

---
+++ src/chapter03/case13/Holder.java
@@ -0,0 +1,17 @@
+package chapter03.case13;
+
+public class Holder {
+
+    private int n;
+
+    public Holder(int n) {
+        this.n = n;
+    }
+
+    public void assertSanity() {
+        if (n != n) {
+            throw new AssertionError("This statement is false");
+        }
+    }
+
+}
