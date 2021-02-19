f22b7cd553c4b5a167de3569a09f4af9e8b9eddd
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-19 18:23:12 +0800

    Create ComposingObject.java

---
+++ src/chapter04/ComposingObject.java
@@ -0,0 +1,21 @@
+package chapter04;
+
+/**
+ * base elements for design a thread safe class
+ * 设计一个安全线程安全的类的基本元素
+ *
+ * identify the variables that form the object's state
+ * 确定来自对象状态的变量
+ * identify the invariables the constrain the state variables
+ * 确定状态变量之间的约束
+ * establish a policy for managing concurrent access to the object's state
+ * 建立并发访问状态的策略
+ * Documenting Synchronization Policies
+ * 以文档的形式记录说明同步策略
+ *
+ * reusing current library (add, extend, delegate) is a good choice to design a thread safe class
+ * 设计一个线程安全的类：复用目前的代码库（直接添加，继承，委托）是一个不错的选择
+ *
+ */
+public class ComposingObject {
+}
