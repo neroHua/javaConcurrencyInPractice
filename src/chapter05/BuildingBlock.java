a2d46a58838393a8677b133a63e6cfcc86eaf740
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-21 12:55:01 +0800

    Update BuildingBlock.java

--- src/chapter05/BuildingBlock.java
+++ src/chapter05/BuildingBlock.java
@@ -1,4 +1,9 @@
 package chapter05;
 
+/**
+ * LockSupport.unpark use sun.misc.Unsafe.unpark to unblock thread
+ * LockSupport.park use sun.misc.Unsafe.park to block thread
+ *
+ */
 public class BuildingBlock {
 }
