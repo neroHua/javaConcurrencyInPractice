bea59ae2f8a01e0796ae8bf3d72465f33ffd7834
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-21 13:01:59 +0800

    Create Memory.java

---
+++ src/chapter05/case11/Memory.java
@@ -0,0 +1,46 @@
+package chapter05.case11;
+
+import common.GuardedBy;
+
+import java.math.BigInteger;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Memory {
+
+    public interface Computable<A, V> {
+
+        V compute(A arg) throws InterruptedException;
+
+    }
+
+    public class ExpensiveFunction implements Computable<String, BigInteger> {
+
+        public BigInteger compute(String arg) {
+            return new BigInteger(arg);
+        }
+
+    }
+
+    public class Memorizer<A, V> implements Computable<A, V> {
+
+        @GuardedBy("this")
+        private final Map<A, V> cache = new ConcurrentHashMap<A, V>();
+        private final Computable<A, V> c;
+
+        public Memorizer(Computable<A, V> c) {
+            this.c = c;
+        }
+
+        public synchronized V compute(A arg) throws InterruptedException {
+            V result = cache.get(arg);
+            if (result == null) {
+                result = c.compute(arg);
+                cache.put(arg, result);
+            }
+            return result;
+        }
+
+    }
+
+}
