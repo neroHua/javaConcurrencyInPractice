fb2961283b2f351c15a0e5c15e994fd0c1940625
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-22 15:34:02 +0800

    Create SingleThreadRenderer.java

---
+++ src/chapter06/case08/SingleThreadRenderer.java
@@ -0,0 +1,41 @@
+package chapter06.case08;
+
+import com.sun.scenario.effect.ImageData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SingleThreadRenderer {
+
+    void renderPage(CharSequence source) {
+        renderText(source);
+        List<ImageData> imageData = new ArrayList<ImageData>();
+
+        for (ImageInfo imageInfo : scanForImageInfo(source)) {
+            imageData.add(imageInfo.downloadImage());
+        }
+
+        for (ImageData data : imageData) {
+            renderImage(data);
+        }
+    }
+
+    private void renderImage(ImageData data) {
+    }
+
+    private ImageInfo[] scanForImageInfo(CharSequence source) {
+        return new ImageInfo[0];
+    }
+
+    private void renderText(CharSequence source) {
+    }
+
+    private class ImageInfo {
+
+        public ImageData downloadImage() {
+            return null;
+        }
+
+    }
+
+}
