66b0b18659b29567d761ac556aca460a56601757
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-23 10:54:36 +0800

    Create BoundedService.java

---
+++ src/chapter07/case15/BoundedService.java
@@ -0,0 +1,36 @@
+package chapter07.case15;
+
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class BoundedService {
+
+    boolean checkMail(Set<String> hosts, long timeout, TimeUnit unit) throws InterruptedException {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        final AtomicBoolean hasNewMail = new AtomicBoolean(false);
+        try {
+            for (final String host : hosts)
+                exec.execute(new Runnable() {
+
+                    public void run() {
+                        if (checkMail(host)) {
+                            hasNewMail.set(true);
+                        }
+                    }
+
+                    private boolean checkMail(String host) {
+                        return false;
+                    }
+
+                });
+        } finally {
+            exec.shutdown();
+            exec.awaitTermination(timeout, unit);
+        }
+
+        return hasNewMail.get();
+    }
+}
