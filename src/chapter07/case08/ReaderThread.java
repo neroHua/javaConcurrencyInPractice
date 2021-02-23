fac250552b28dbd2325791dc2fb5cb5abf1757d1
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-23 10:52:29 +0800

    Create ReaderThread.java

---
+++ src/chapter07/case08/ReaderThread.java
@@ -0,0 +1,50 @@
+package chapter07.case08;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class ReaderThread extends Thread {
+
+    private final Socket socket;
+    private final InputStream in;
+
+    public ReaderThread(Socket socket) throws IOException {
+        this.socket = socket;
+        this.in = socket.getInputStream();
+    }
+
+    @Override
+    public void interrupt() {
+        try {
+            socket.close();
+        }
+        catch (IOException ignored) {
+        }
+        finally {
+            super.interrupt();
+        }
+    }
+
+    public void run() {
+        try {
+            byte[] buf = new byte[1024];
+            while (true) {
+                int count = in.read(buf);
+                if (count < 0) {
+                    break;
+                }
+                else if (count > 0) {
+                    processBuffer(buf, count);
+                }
+            }
+        }
+        catch (IOException e) {
+            /* Allow thread to exit */
+        }
+    }
+
+    private void processBuffer(byte[] buf, int count) {
+    }
+
+}
