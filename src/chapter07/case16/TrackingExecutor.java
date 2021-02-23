bd8365798b9994d20afbe6a97ffd511c216f5874
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-23 10:54:54 +0800

    Create TrackingExecutor.java

---
+++ src/chapter07/case16/TrackingExecutor.java
@@ -0,0 +1,62 @@
+package chapter07.case16;
+
+import java.util.*;
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class TrackingExecutor extends AbstractExecutorService {
+
+    private final ExecutorService exec;
+    private final Set<Runnable> tasksCancelledAtShutdown = Collections.synchronizedSet(new HashSet<Runnable>());
+
+    public TrackingExecutor(ExecutorService exec) {
+        this.exec = exec;
+    }
+
+    public List<Runnable> getCancelledTasks() {
+        if (!exec.isTerminated()) {
+            throw new IllegalStateException();
+        }
+        return new ArrayList<Runnable>(tasksCancelledAtShutdown);
+    }
+
+    public void execute(final Runnable runnable) {
+        exec.execute(new Runnable() {
+            public void run() {
+                try {
+                    runnable.run();
+                } finally {
+                    if (isShutdown() && Thread.currentThread().isInterrupted()) {
+                        tasksCancelledAtShutdown.add(runnable);
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void shutdown() {
+        exec.shutdown();
+    }
+
+    @Override
+    public List<Runnable> shutdownNow() {
+        return exec.shutdownNow();
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return exec.isShutdown();
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return exec.isTerminated();
+    }
+
+    @Override
+    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+        return exec.awaitTermination(timeout, unit);
+    }
+}
