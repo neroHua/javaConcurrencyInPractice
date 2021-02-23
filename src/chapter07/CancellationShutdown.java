a97f25c811d1fba7ef76f0d2e4ab016936ef0732
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-23 10:49:10 +0800

    Create CancellationShutdown.java

---
+++ src/chapter07/CancellationShutdown.java
@@ -0,0 +1,14 @@
+package chapter07;
+
+/**
+ * Java does not provide any mechanism for safely forcing a thread to stop what it is doing
+ * java中没有安全的强制停止线程的的机制
+ *
+ * Thread interruption is a cooperative mechanism
+ * 线程中断是一种合作机制
+ * for a thread to signal another thread that it should, at its convenience and if it feels like it, stop what it is doing and do something else.
+ * 一个线程通知另外一个线程停止它正在做的事情，在它方便和可以的时候
+ *
+ */
+public class CancellationShutdown {
+}
