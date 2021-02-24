0b783329a57c1b7c4a4836d8e3d3913f17306663
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-24 15:15:39 +0800

    Create ApplyingThreadPools.java

---
+++ src/chapter08/ApplyingThreadPools.java
@@ -0,0 +1,18 @@
+package chapter08;
+
+/**
+ * threadPool
+ * 线程池
+ *
+ * flyweight pattern
+ * 享元模式
+ *
+ * producer and consumer pattern
+ * 生产者消费者模式
+ *
+ * a sets threads get task form queue and execute task ceaselessly
+ * 一系列的线程不断的从队列中获取任务，并执行任务
+ *
+ */
+public class ApplyingThreadPools {
+}
