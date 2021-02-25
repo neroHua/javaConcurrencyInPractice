502b7cc9a1cb354e947457f23ba136296e0a97fa
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-25 11:24:16 +0800

    Create AvoidingLivenessHazard.java

---
+++ src/chapter10/AvoidingLivenessHazard.java
@@ -0,0 +1,15 @@
+package chapter10;
+
+/**
+ * 1. dead lock
+ *  死锁
+ * 2. live lock
+ *  活锁
+ * 3. thread starvation
+ *  线程饥饿
+ * 4. poor response
+ *  响应性能差
+ *
+ */
+public class AvoidingLivenessHazard {
+}
