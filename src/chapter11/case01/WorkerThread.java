366af2717eaa544070df29acf0ecd934ae0a2e94
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-25 15:26:48 +0800

    Create WorkerThread.java

---
+++ src/chapter11/case01/WorkerThread.java
@@ -0,0 +1,24 @@
+package chapter11.case01;
+
+import java.util.concurrent.BlockingQueue;
+
+public class WorkerThread extends Thread {
+
+    private final BlockingQueue<Runnable> queue;
+
+    public WorkerThread(BlockingQueue<Runnable> queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                Runnable task = queue.take();
+                task.run();
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+
+}
