2899a6c7c97dc11fb0313eb102b2d2193d68e366
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-02-26 15:29:48 +0800

    Create ExplicitLocks.java

---
+++ src/chapter13/ExplicitLocks.java
@@ -0,0 +1,19 @@
+package chapter13;
+
+/**
+ * when to use explicit lock ?
+ * 什么时候使用显示锁?
+ *
+ * 1. timed
+ * 1. 定时的
+ * 2. polled
+ * 2. 可轮训的
+ * 3. interrupt
+ * 3. 可响应中断的
+ * 4. fair lock and unfair lock
+ * 4. 需要公平锁和非公平锁
+ * 5. no-block-structure
+ * 5. 不是块结构
+ */
+public class ExplicitLocks {
+}
